e1ad2e17871c2b7896c477b7819b74d9fa0bf555
Muhammad Ali <alikhancss27@gmail.com>
2024-11-21 18:48:13 +0500

    post

--- app.js
+++ app.js
@@ -27,11 +27,11 @@ function submit() {
          <div class="card-header d-flex flex-column flex-sm-row flex=wrap justify-content-between fw-bold" style="color: rgb(31, 160, 235) !important;">
          <span> ~@${userName.value}</span> <span> ${datetoday} </span>
          </div>
-         <div class= "text-uppercase text-center ms-2" style="border-bottom: 1px solid lightgrey  !important"; color:rgb(31, 160, 235) !important>
-          <h4>TITLE : ${postTitle.value}:</h4>
+         <div class= "text-uppercase text-center " style="border-bottom: 1px solid lightgrey  !important; color: rgb(7, 1, 92)!important;">
+          <h5 class="ms-2">TITLE : ${postTitle.value}</h5>
          </div>
          <div class="special-div pb-5" style="background:url(${selectedimage})">
-         <span class="ms-5">${thought.value}</span>
+         <span class=" ms-2 me-2">${thought.value}</span>
          </div>
       </div> `
        userName.value=""
