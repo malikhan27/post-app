64e8fc35a79b76eb01d7d5894985fe97a83b2150
Muhammad Ali <alikhancss27@gmail.com>
2024-11-22 18:13:13 +0500

    img-div add

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 var selectedimage;
+var titleimageselect;
 function imageselector(src) {
   backGroundimage = document.getElementsByClassName("bg-image");
 
@@ -9,6 +10,18 @@ function imageselector(src) {
   event.target.classList.add("select");
   selectedimage = src;
   console.log(selectedimage)
+}
+
+function titleimage(src) {
+  var titleimagediv = document.getElementsByClassName("title-img");
+
+ for(j=0 ; j<titleimagediv.length;j++){
+  titleimagediv[j].className="title-img"
+ }
+
+  event.target.classList.add("select");
+  titleimageselect = src;
+  console.log(titleimageselect)
 }
 
 function submit() {
@@ -28,13 +41,15 @@ function submit() {
          <div class="card-header d-flex flex-column flex-sm-row flex=wrap justify-content-between fw-bold" style="color: rgb(31, 160, 235) !important;">
          <span> ~@${userName.value}</span> <span> ${datetoday} </span>
          </div>
-         <div class= "text-uppercase text-center " style="border-bottom: 1px solid lightgrey  !important; color: rgb(7, 1, 92)!important;">
+         <div >
+         <div class= "text-uppercase text-center title-div" 
+         style="border-bottom: 1px solid lightgrey  !important; color: rgb(7, 1, 92)!important;background:url(${titleimageselect}) !important">
           <h5 class="ms-2">TITLE : ${postTitle.value}</h5>
          </div>
          <div class="special-div pb-5" style="background:url(${selectedimage})">
          <span class=" ms-2 me-2">${thought.value}</span>
          </div>
-      </div> `
+         </div>`
        userName.value=""
        postTitle.value=""
        thought.value= ""
